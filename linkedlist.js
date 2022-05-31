55a774418f2aae080b4c0a457e1d4770fe2adccb
Joseph Nii Lante Lamptey <niilantewade77@gmail.com>
2022-05-31 04:22:53 +0000

    add to head and tail node methods

--- linkedlist.js
+++ linkedlist.js
@@ -1,4 +1,4 @@
- // perform add, remove,retrieve and search operation on a linked list
+   // perform add, remove,retrieve and search operation on a linked list
 
 function linkedList () {
  this.head = null
@@ -19,23 +19,124 @@ function Node(value, next, prev){
 // console.log(node1)
 
 
-// add node
+// add node to head 
 
 linkedList.prototype.addtoHead = function (value){
 
   //create new node
   const newNode = new Node (value, this.head, null)
 
+  // if linked list has nodes present  
   this.head ? this.head.prev = newNode : this.tail = newNode
 
   this.head = newNode
 }
 
 
+ 
+// const link = new linkedList ()
 
-const ll = new linkedList ()
+// link.addtoHead(100)
+// link.addtoHead(300)
+// link.addtoHead(600) 
 
-ll.addtoHead(100)
-ll.addtoHead(300)
+// console.log(link) 
 
-console.log(ll)
+
+//add node to tail
+
+
+linkedList.prototype.addtoTail = function(value){
+
+  //add new node
+const newNode = new Node(value, null, this.tail )
+
+//check if there node is present
+this.tail ? this.tail.next = newNode : this.head = newNode
+
+this.tail = newNode
+  
+}
+
+// const links = new linkedList()
+
+// links.addtoTail(100)
+// links.addtoTail(200)
+// links.addtoTail(300)
+// links.addtoHead(400)
+
+// console.log(links.tail.prev.prev)
+
+
+
+// remove node from head and return value
+
+
+linkedList.prototype.removeHead = function (){
+
+  //if linkedlist is empty
+
+  if (!this.head) return null
+
+  // if linked is not empty
+
+  const val = this.head.value
+
+  this.head = this.head.next
+
+  this.head ? this.head.prev =null : this.tail =null
+
+  return val
+  
+  
+}
+
+
+// const link = new linkedList()
+
+// link.addtoHead(100)
+// link.addtoHead(200)
+// link.addtoTail(300)
+
+// link.removeHead()
+
+// console.log(link)
+
+
+//remove node from tail and return its value
+
+linkedList.prototype.removeTail = function(){
+
+  // if linkedlist is empty
+  if (!this.tail) return null
+
+  // if linked list is not empty
+  const val = this.tail.value
+
+  this.tail = this.tail.prev
+
+  this.tail ? this.tail.next = null : this.head = null
+
+  return val
+}
+
+
+// const links = new linkedList()
+
+// links.addtoHead(100)
+// links.addtoHead(200)
+// links.addtoTail(300)
+
+// links.removeTail()
+
+// console.log(links)
+
+
+// Search
+
+
+linkedList.prototype.search = function(searchValue){
+
+ 
+  
+}
